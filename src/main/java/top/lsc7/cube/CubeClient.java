5efdab73b7c91287dcd0eeb1d11903cbba57df62
小方盒LSCube <ck20130914@outlook.com>
2023-10-06 14:05:53 +0800

    Update

---
+++ src/main/java/top/lsc7/cube/CubeClient.java
@@ -0,0 +1,10 @@
+package top.lsc7.cube;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CubeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/top/lsc7/cube/CubeDataGenerator.java
@@ -0,0 +1,11 @@
+package top.lsc7.cube;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CubeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
